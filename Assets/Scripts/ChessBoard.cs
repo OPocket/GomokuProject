cf76211b2dc64d64580e91a478d80757eb323757
OPocket <595771629@qq.com>
2019-10-22 23:24:58 +0800

    GomokuProject

---
+++ Assets/Scripts/ChessBoard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessBoard : MonoBehaviour
+{
+    public ChessType curTurn= ChessType.Black;
+
+    // 地图格子
+    public int Max_LINE= 15;
+    private int[,] grid;
+
+    private bool isGameOver = false;
+
+    public enum ChessType
+    {
+        Watch,
+        White,
+        Black
+    }
+
+    private void Awake()
+    {
+        grid = new int[Max_LINE, Max_LINE];
+    }
+
+    private void Update()
+    {
+        if (isGameOver) return;
+    }
+
+    private void PlayChess()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public ChessBoard.ChessType chessType = ChessBoard.ChessType.Black;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        CheckClickPos();
+    }
+
+    // 获取点击的棋盘坐标位置
+    private Vector2 CheckClickPos()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            return new Vector2(Mathf.RoundToInt(clickPos.x + 7), Mathf.RoundToInt(clickPos.y + 7));
+        }
+        return null;
+    }
+}
